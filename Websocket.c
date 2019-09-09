bedc032ccd682fe0b7c41d0ee90faa53d91e8e71
vk <liu.bao.long.vk@gmail.com>
2019-09-09 19:28:06 +0800

    Fix: lib

--- Websocket.c
+++ Websocket.c
@@ -1011,8 +1011,8 @@ int start_websocket_server(struct IWebsocket *iwebsocket) {
         return EXIT_SUCCESS;
 }
 
-// #ifdef TEST_MAIN_PC
-#if 1
+#ifdef TEST_MAIN_PC
+// #if 1
 // 不要使用cJSON的动态库，可能存在如下问题：
 // 1. 动态库全局变量s2jHook的问题
 // S2jHook s2jHook = {
