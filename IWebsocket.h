fedaef430cfb3fb52f84b41f7ed5916de9f4b811
vk <liu.bao.long.vk@gmail.com>
2019-09-10 13:38:12 +0800

    Fix: IWebsocket.h

--- IWebsocket.h
+++ IWebsocket.h
@@ -1,6 +1,8 @@
 #ifndef _IWebsocket_H
 #define _IWebsocket_H
 #include "Datastructures.h"
+#include "Communicate.h"
+#include "Handshake.h"
 
 struct IWebsocket{
     int  port;
@@ -16,7 +18,7 @@ struct IWebsocket{
  * so just send text, message_new() is new a text message.
  */
 extern int ws_send_text(ws_client *wsclient, char *text);
-extern int ws_send_text_all_client(char *text);
+extern int ws_send_text_all(char *text);
 extern int start_websocket_server(struct IWebsocket *iwebsocket);
 extern const ws_list *  ws_get_clients_list();
 
